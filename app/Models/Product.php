f35131e0921c61e4a2a578a923186565dff69bef
Tuan Tu <tuantuchautrinh@gmail.com>
2019-12-11 23:40:44 +0700

    Eloquent models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // #Eloquent ORM - Getting Started #Defining Models #Eloquent Model Conventions - Table Names
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'my_flights';
+
+    // #Eloquent ORM - Getting Started #Defining Models #Inserting & Updating Models #Mass Assignment
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    // fillable bao gồm các cột được phép tương tác đến
+    protected $fillable = ['name'];
 }
